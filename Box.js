dc6d866d89d4cee186968612c28d482b8638b611
Ayaan-Shaikh <78639533+Ayaan-Shaikh@users.noreply.github.com>
2021-02-27 14:50:58 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,16 @@
+class Box{
+    constructor(x,y,width,height){
+        var options=
+        {restitution:1.5}
+        
+        this.body=Bodies.rectangle(x,y,width,height);
+        this.width=width
+        this.height=height
+        World.add(world,this.body)
+    }
+display(){
+    fill(255)
+      rectMode(CENTER);
+    rect(this.body.position.x,this.body.position.y,this.width,this.height)
+}
+}
